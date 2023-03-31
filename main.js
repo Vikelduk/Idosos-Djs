3d6ba4d9a998fcc8d35aadc5d698221397022bc5
Vikelduk <108426561+Vikelduk@users.noreply.github.com>
2023-03-31 16:53:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var song1 = "";
-var song2 = "";
-
+var song2 = ""
 var tocando = false;
+
+var rightWristyX = 0;
+var rightWristyY = 0;
+
+var leftWristyX = 0;
+var leftWristyY = 0;
 
 function preload()
 {
@@ -11,14 +16,50 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(600, 500);
-    canvas.position(375, 190);
+    canvas = createCanvas(550, 500);
+    canvas.position(390, 190);
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("O modelo PoseNet foi inicializado com sucesso!");
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+
+        leftWristyX = results[0].pose.leftWrist.x;
+        leftWristyY = results[0].pose.leftWrist.y;
+        console.log("LeftWridtX = " + leftWristyX + "LeftWristY = " + leftWristyY);
+
+        rightWristyX = results[0].pose.rightWrist.x;
+        rightWristyX = results[0].pose.rightWrist.y;
+        console.log("RightWridtX = " + rightWristyX + "RightWristY = " + rightWristyY);
+    }
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    if (tocando == false)
+    {
+        song2.play();
+        tocando = true
+        song2.setVolume(0.5);
+        song2.rate(1);
+    }
+
 }
